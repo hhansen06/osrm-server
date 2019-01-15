ab6785dccfd13834ad930057a29428f4eadc3093
hhansen06 <hhansen06@googlemail.com>
2019-01-16 00:46:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,12 @@ if [ ! -f /home/renderer/osrm/firststart ]; then
         wget -nv http://download.geofabrik.de/europe/germany/niedersachsen-latest.osm.pbf -O /home/renderer/osrm/data.osm.pbf
       
     fi
+    
     cd /home/renderer/osrm/
     osrm-extract -p /home/renderer/osrm/profile.lua  /home/renderer/osrm/data.osm.pbf
     osrm-partition data.osrm
-    osrm-customize data.osrm
-    osrm-routed --algorithm mld /data/berlin-latest.osrm
-    
-      touch /home/renderer/osrm/firststart
+    osrm-customize data.osrm    
+    touch /home/renderer/osrm/firststart
 fi
 
 osrm-routed --algorithm mld data.osrm
