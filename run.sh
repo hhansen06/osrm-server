304126d7266c78a0de8b3c0d6956d6acba8fef34
hhansen06 <hhansen06@googlemail.com>
2019-01-16 00:46:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ if [ ! -f /home/renderer/osrm/firststart ]; then
     if [ ! -f /home/renderer/osm/data.osm.pbf ]; then
         echo "WARNING: No import file at /data.osm.pbf, so importing Niedersachsen as example..."
         wget -nv http://download.geofabrik.de/europe/germany/niedersachsen-latest.osm.pbf -O /home/renderer/osrm/data.osm.pbf
-        osrm-extract /home/renderer/osrm/data.osm.pbf
+      
     fi
     cd /home/renderer/osrm/
-    osrm-extract map.osm
-
-    
+    osrm-extract -p /home/renderer/osrm/profile.lua  /home/renderer/osrm/data.osm.pbf
+    osrm-partition data.osrm
+    osrm-customize data.osrm
+    osrm-routed --algorithm mld /data/berlin-latest.osrm
     
       touch /home/renderer/osrm/firststart
 fi
 
-
-/usr/local/bin/osrm-routed -p 5000 map.osrm
-exit 1
+osrm-routed --algorithm mld data.osrm
+exit 0
